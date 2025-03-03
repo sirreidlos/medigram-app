a04e579f661a475a8b7bdff52f542511791f07d9
davitak <davitakhowati@gmail.com>
2025-03-03 19:16:14 +0700

    Feat: Home page user type

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:medigram_app/components/record_card.dart';
 import 'package:medigram_app/components/show_qr.dart';
-import 'package:medigram_app/utils/qr_image.dart';
 import 'package:medigram_app/utils/style.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   final String uniqueCode = "12345";
+  final bool isPatient = true;
 
   @override
   Widget build(BuildContext context) {
@@ -30,113 +30,18 @@ class HomePage extends StatelessWidget {
                 children: [
                   Row(
                     children: [
-                      Text("Hello, Jane Doe", style: TextStyle(fontSize: 18)),
+                      Text(
+                        "Hello, ${isPatient ? "" : "Dr. "} Jane Doe",
+                        style: TextStyle(fontSize: 18),
+                      ),
                       Spacer(),
                       Icon(Icons.swap_horiz_rounded),
                       Icon(Icons.notifications),
                     ],
                   ),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () async {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: ((context) {
-                              return ShowQr(uniqueCode, true);
-                            }),
-                          ),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color(primaryColor1),
-                        padding: EdgeInsets.only(
-                          top: 30,
-                          bottom: 30,
-                          left: 0,
-                          right: 0,
-                        ),
-                        foregroundColor: Colors.black,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                      ),
-                      child: Text(
-                        "Start Consultation",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Row(
-                    spacing: 20,
-                    children: [
-                      Expanded(
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: ((context) {
-                                  return ShowQr(uniqueCode, false);
-                                }),
-                              ),
-                            );
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Color(primaryColor2),
-                            padding: EdgeInsets.only(
-                              top: 30,
-                              bottom: 30,
-                              left: 0,
-                              right: 0,
-                            ),
-                            foregroundColor: Colors.white,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                          ),
-                          child: Text(
-                            "Meds Claim",
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w600,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Color(secondaryColor2),
-                            padding: EdgeInsets.only(
-                              top: 30,
-                              bottom: 30,
-                              left: 0,
-                              right: 0,
-                            ),
-                            foregroundColor: Colors.white,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                          ),
-                          child: Text(
-                            "Meds Regimen",
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w600,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                  isPatient ? patientConsult(context, uniqueCode) : Scaffold(),
+                  isPatient ? medsHandler(context, uniqueCode) : Scaffold(),
+
                 ],
               ),
             ),
@@ -188,4 +93,89 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
+}
+
+Widget doctorScan(){
+  return Scaffold();
+}
+
+Widget patientConsult(BuildContext context, String uniqueCode) {
+  return SizedBox(
+    width: double.infinity,
+    child: ElevatedButton(
+      onPressed: () async {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: ((context) {
+              return ShowQr(uniqueCode, true);
+            }),
+          ),
+        );
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Color(primaryColor1),
+        padding: EdgeInsets.only(top: 30, bottom: 30, left: 0, right: 0),
+        foregroundColor: Colors.black,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      ),
+      child: Text(
+        "Start Consultation",
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+      ),
+    ),
+  );
+}
+
+Widget medsHandler(BuildContext context, String uniqueCode) {
+  return Row(
+    spacing: 20,
+    children: [
+      Expanded(
+        child: ElevatedButton(
+          onPressed: () async {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: ((context) {
+                  return ShowQr(uniqueCode, false);
+                }),
+              ),
+            );
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Color(primaryColor2),
+            padding: EdgeInsets.only(top: 30, bottom: 30, left: 0, right: 0),
+            foregroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: Text(
+            "Meds Claim",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+      Expanded(
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Color(secondaryColor2),
+            padding: EdgeInsets.only(top: 30, bottom: 30, left: 0, right: 0),
+            foregroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: Text(
+            "Meds Regimen",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    ],
+  );
 }
