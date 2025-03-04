07ee6d8223bd45b8ddc3188e7856d97da10dcb9c
davitak <davitakhowati@gmail.com>
2025-03-04 21:12:06 +0700

    Feat: Scanner UI

--- lib/components/scan_qr.dart
+++ lib/components/scan_qr.dart
@@ -9,43 +9,80 @@ class ScanQR extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(screenPadding),
-      decoration: BoxDecoration(
-        color: Color(secondaryColor1),
-        borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(40),
-          bottomRight: Radius.circular(40),
-        ),
-      ),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 300,
-            width: 300,
-            child: MobileScanner(
-              scanWindow: Rect.fromLTWH(screenPadding, screenPadding, 300, 300),
-              controller: MobileScannerController(
-                detectionSpeed: DetectionSpeed.noDuplicates
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(screenPadding), // TODO: Fix size
+              decoration: BoxDecoration(
+                color: Color(secondaryColor1),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(40),
+                  bottomRight: Radius.circular(40),
+                ),
               ),
-              onDetect: (capture) {
-                final List<Barcode> barcodes = capture.barcodes;
-                String barcode = "";
-                for (final b in barcodes) {
-                  barcode = barcode + (b.rawValue ?? "");
-                }
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: ((context) {
-                      return ConsultForm(barcode);
-                    }),
+              child: Column(
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: Icon(Icons.arrow_back, color: Colors.black),
+                      ),
+                      Text(
+                        "Scan Patient Data",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
                   ),
-                );
-              },
+                  Container(
+                    padding: EdgeInsets.only(
+                      top: screenPadding,
+                      bottom: screenPadding,
+                    ),
+                    child: SizedBox(
+                      height: 300,
+                      width: 300,
+                      child: MobileScanner(
+                        scanWindow: Rect.fromLTWH(
+                          screenPadding,
+                          screenPadding,
+                          300,
+                          300,
+                        ),
+                        controller: MobileScannerController(
+                          detectionSpeed: DetectionSpeed.noDuplicates,
+                        ),
+                        onDetect: (capture) {
+                          final List<Barcode> barcodes = capture.barcodes;
+                          String barcode = "";
+                          for (final b in barcodes) {
+                            barcode = barcode + (b.rawValue ?? "");
+                          }
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: ((context) {
+                                return ConsultForm(barcode);
+                              }),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
